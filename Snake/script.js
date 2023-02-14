48593458692a325c0dadaddd84f84209d0b290f8
bishojbk <bishojbk@gmail.com>
2023-02-14 12:29:52 +0545

    Snake-Game

---
+++ Snake/script.js
@@ -0,0 +1,93 @@
+const playingBoard = document.querySelector(".playing-board");
+let scoreCard = document.querySelector(".score");
+let highScoreCard = document.querySelector(".high-score");
+
+let foodX, foodY;
+let snakeX = 5,
+  snakeY = 10;
+
+let velocityX = 0,
+  velocityY = 0;
+
+let snakeBody = [];
+let gameOver = false;
+let setIntervalId;
+let score = 0;
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreCard.innerHTML = `High Score: ${highScore}`;
+
+const changeFoodPosition = () => {
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
+
+const handleGameOver = () => {
+  clearInterval(setIntervalId);
+  alert("Game Over. Click Ok if you want to continue.");
+  location.reload();
+};
+
+const changeDirection = (e) => {
+  if (e.key === "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  }
+  if (e.key === "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  }
+  if (e.key === "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+  initGame();
+};
+
+const initGame = () => {
+  if (gameOver) return handleGameOver();
+  let template = `<div class="food" style="grid-area:${foodY}/${foodX}"></div>`;
+
+  if (snakeX === foodX && snakeY === foodY) {
+    changeFoodPosition();
+    snakeBody.push([foodX, foodY]);
+    score++;
+    highScore = score >= highScore ? score : highScore;
+    localStorage.setItem("high-score", highScore);
+    scoreCard.innerHTML = `Score: ${score}`;
+    highScoreCard.innerHTML = `High Score: ${highScore}`;
+  }
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+
+  snakeBody[0] = [snakeX, snakeY];
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    template += `<div class="snake" style="grid-area:${snakeBody[i][1]}/${snakeBody[i][0]}"></div>`;
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
+    }
+  }
+
+  playingBoard.innerHTML = template;
+};
+
+changeFoodPosition();
+setIntervalId = setInterval(initGame, 150);
+initGame();
+
+document.addEventListener("keydown", changeDirection);
